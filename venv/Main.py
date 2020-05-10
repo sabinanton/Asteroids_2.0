93b0548ca7b66cec9018c85815f113e3e8ac8f5f
Sabin Anton <sabinanton@gmail.com>
2020-05-10 20:50:04 +0200

    Added Mousewheel control

--- venv/Main.py
+++ venv/Main.py
@@ -27,13 +27,13 @@ def generate_Asteroid_Belt(radius, number_of_ast):
     return AstList
 
 
-Earth = Celestial_bodies.Planet("Earth", 5.97219*10**23, 63710000, 147098070000, 0, 0, 30280)
+Earth = Celestial_bodies.Planet("Earth", 5.97219*10**24, 63710000, 147098070000, 0, 0, 30280)
 Sun = Celestial_bodies.Planet("Sun", 1.989*10**30, 6963400000, 0, 0, 0, 0)
-Mars = Celestial_bodies.Planet("Mars", 6.39*10**22, 33895000, 1.38*Constants.AU, 0, 0, 26500)
+Mars = Celestial_bodies.Planet("Mars", 6.39*10**23, 33895000, 1.38*Constants.AU, 0, 0, 26500)
 #Jupiter = Celestial_bodies.Planet("Jupiter", 1.898*10**27, 699110000, 5.034*Constants.AU, 0, 0, 13720)
 ast = generate_Asteroid_Belt(329115316000, 1665)
-#Moon = Celestial_bodies.Planet("Moon", 7.34767*10**22, 1737000, 147098070000 + 384400000, 0, 0, 61298)
-sim = Simulation_tools.Simulation([Earth, Sun, Mars],ast, 329945)
+Moon = Celestial_bodies.Planet("Moon", 7.34767*10**22, 17370000, 147098070000 + 384400000, 0, 0, 31298)
+sim = Simulation_tools.Simulation([Earth, Sun, Mars],ast, 15555)
 pygame.init()
 
 resolution = (1080,720)
@@ -44,10 +44,9 @@ running = True
 i=0
 
 while running :
-    i+=1
     sim.simulate()
-    x_offset = -Constants.scale * ast[4].pos_x
-    y_offset = Constants.scale * ast[4].pos_y
+    x_offset = -Constants.scale * Earth.pos_x
+    y_offset = Constants.scale * Earth.pos_y
     pygame.draw.rect(screen, black, screct)
     Earth.draw(resolution,screen, x_offset, y_offset)
     Sun.draw(resolution, screen, x_offset, y_offset)
@@ -57,17 +56,26 @@ while running :
     for j in ast:
         j.draw(resolution,screen, x_offset, y_offset)
     pygame.display.flip()
-    if i >69000: running = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            running = False
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
+                running = False
                 pygame.QUIT
                 sys.exit()
         if event.type == pygame.VIDEORESIZE:
             resolution = (event.w, event.h)
             screen = pygame.display.set_mode(resolution, pygame.RESIZABLE)
             screct = screen.get_rect()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            scale_change = 0.9
+            if event.button == 4:
+                Constants.scale /= scale_change
+            if event.button == 5:
+                Constants.scale *= scale_change
+
+
 
