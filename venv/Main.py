1b9fffe2c62859c22b02707d0da820ab6ff724b5
Sabin Anton <sabinanton@gmail.com>
2020-05-10 19:14:02 +0200

    Added resizable window option + possibility to exit with escape key or X button.

--- venv/Main.py
+++ venv/Main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, sys
 import random
 import math
 from pygame import gfxdraw
@@ -27,17 +27,17 @@ def generate_Asteroid_Belt(radius, number_of_ast):
     return AstList
 
 
-Earth = Celestial_bodies.Planet("Earth", 5.97219*10**24, 63710000, 147098070000, 0, 0, 30280)
+Earth = Celestial_bodies.Planet("Earth", 5.97219*10**23, 63710000, 147098070000, 0, 0, 30280)
 Sun = Celestial_bodies.Planet("Sun", 1.989*10**30, 6963400000, 0, 0, 0, 0)
-Mars = Celestial_bodies.Planet("Mars", 6.39*10**23, 33895000, 1.38*Constants.AU, 0, 0, 26500)
+Mars = Celestial_bodies.Planet("Mars", 6.39*10**22, 33895000, 1.38*Constants.AU, 0, 0, 26500)
 #Jupiter = Celestial_bodies.Planet("Jupiter", 1.898*10**27, 699110000, 5.034*Constants.AU, 0, 0, 13720)
-ast = generate_Asteroid_Belt(329115316000, 2665)
+ast = generate_Asteroid_Belt(329115316000, 1665)
 #Moon = Celestial_bodies.Planet("Moon", 7.34767*10**22, 1737000, 147098070000 + 384400000, 0, 0, 61298)
-sim = Simulation_tools.Simulation([Earth, Sun, Mars],ast, 99945)
+sim = Simulation_tools.Simulation([Earth, Sun, Mars],ast, 329945)
 pygame.init()
 
 resolution = (1080,720)
-screen = pygame.display.set_mode(resolution)
+screen = pygame.display.set_mode(resolution, pygame.RESIZABLE)
 screct = screen.get_rect()
 black = (0,0,0)
 running = True
@@ -46,8 +46,8 @@ i=0
 while running :
     i+=1
     sim.simulate()
-    x_offset = -Constants.scale * ast[5].pos_x
-    y_offset = Constants.scale * ast[5].pos_y
+    x_offset = -Constants.scale * ast[4].pos_x
+    y_offset = Constants.scale * ast[4].pos_y
     pygame.draw.rect(screen, black, screct)
     Earth.draw(resolution,screen, x_offset, y_offset)
     Sun.draw(resolution, screen, x_offset, y_offset)
@@ -58,6 +58,16 @@ while running :
         j.draw(resolution,screen, x_offset, y_offset)
     pygame.display.flip()
     if i >69000: running = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.QUIT
+                sys.exit()
+        if event.type == pygame.VIDEORESIZE:
+            resolution = (event.w, event.h)
+            screen = pygame.display.set_mode(resolution, pygame.RESIZABLE)
+            screct = screen.get_rect()
 
-
-pygame.quit()
